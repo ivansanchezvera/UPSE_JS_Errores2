c34e1bce62891b10dc2b9294ce98f5a9b6c7cc83
JENNY PILAY <nashlyascepilay@hotmail.com>
2016-03-20 15:47:05 -0500

    correccion de errores

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 console.log("Inicio de programa");
-alert("Hola JavaScript);
-document.write("<h1>Hola JS UPSE</p>");
+alert("Hola JavaScript");
+document.write("<h1>Hola JS UPSE</h1>");
 alet("Gracias por visitar");
-console.log("Fin de programa")
+console.log("Fin de programa");
